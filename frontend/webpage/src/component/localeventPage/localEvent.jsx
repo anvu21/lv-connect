824de183b17a7d1a37a3e63ac94d49727bb90957
matthew0122 <mdb326@lehigh.edu>
2023-11-11 15:00:23 -0500

    Add basic event page

--- frontend/webpage/src/component/localeventPage/localEvent.jsx
+++ frontend/webpage/src/component/localeventPage/localEvent.jsx
@@ -2,13 +2,19 @@ import styles from './styles.module.css';
 import React, { useState } from 'react';
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import Event from "./Event.jsx";
 
 const localEvent = () => {
   
  
   
   return (
-    <div>Local
+    <div>
+      <h1>Local Events</h1>
+      <Event name="TestName" date="12/13/2023" location="27 Memorial Dr W, Bethlehem, PA 18015"
+        posted="12/01/2023"/>
+      <Event name="TestName" date="12/13/2023" location="27 Memorial Dr W, Bethlehem, PA 18015"
+        posted="12/01/2023"/>
     </div>
   )
 }

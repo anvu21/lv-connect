06c5fce0dccb15c947f1b378e62e7ee50392b5e3
matthew0122 <mdb326@lehigh.edu>
2023-11-11 15:18:50 -0500

    Get events from object hook

--- frontend/webpage/src/component/localeventPage/localEvent.jsx
+++ frontend/webpage/src/component/localeventPage/localEvent.jsx
@@ -1,23 +1,42 @@
 import styles from './styles.module.css';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import Event from "./Event.jsx";
 import NavBar from "../Navbar.jsx"
 
 const localEvent = () => {
+
+  const [events, setEvents] = useState([]);
+  useEffect( () => {
+    setEvents([...events, {
+      "id": "1",
+      "name": "TestName",
+      "date": "12/13/2023",
+      "location": "27 Memorial Dr W, Bethlehem, PA 18015",
+      "posted": "11/01/2023"
+    },
+    {
+      "id": "2",
+      "name": "TestName2",
+      "date": "01/26/2024",
+      "location": "730 High St, Easton, PA 1804",
+      "posted": "11/10/2023"
+    } ]);
+  }, []);
   
- 
   
   return (
     <>
       <NavBar />
       <div>
         <h1 className='my-5 text-3xl'>Local Events</h1>
-        <Event name="TestName" date="12/13/2023" location="27 Memorial Dr W, Bethlehem, PA 18015"
-          posted="12/01/2023"/>
-        <Event name="TestName" date="12/13/2023" location="27 Memorial Dr W, Bethlehem, PA 18015"
-          posted="12/01/2023"/>
+        {
+          events.map(event => (
+            <Event name={event.name} date={event.date} location={event.location}
+              posted={event.posted} key={event.id}/>
+          ))
+        }
       </div>
     </>
   )

49bbac6486e5aed0327b5c242704e86fdcf32a8c
matthew0122 <mdb326@lehigh.edu>
2023-11-11 15:52:22 -0500

    Add styling to comment

--- frontend/webpage/src/component/localeventPage/EventComment.jsx
+++ frontend/webpage/src/component/localeventPage/EventComment.jsx
@@ -8,10 +8,10 @@ const EventComment = (props) => {
   return (
     <>
     <div className="bg-gray-100 text-left rounded-lg my-3 ml-10" style={{ color: '#00539b' }}>
-        <p className='m-4 py-4 '>{props.content}</p>
+        <p className='mx-4 py-4 '>{props.content}</p>
         <div className="flex justify-between">
-            <p className='m-3'>{props.user}</p>
-            <p className='m-3'>{props.date}</p>
+            <p className='mx-3 mb-1.5'>{props.user}</p>
+            <p className='mx-3 mb-1.5'>{props.date}</p>
         </div> 
     </div>
     

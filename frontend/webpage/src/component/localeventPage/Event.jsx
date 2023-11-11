29dabea56bce5b60354ddd9e1743b0582da35ea9
matthew0122 <mdb326@lehigh.edu>
2023-11-11 15:22:23 -0500

    Add description to objects

--- frontend/webpage/src/component/localeventPage/Event.jsx
+++ frontend/webpage/src/component/localeventPage/Event.jsx
@@ -8,7 +8,7 @@ const Event = (props) => {
   return (
     <div className="bg-gray-100 text-left rounded-lg my-3" style={{ color: '#00539b' }}>
         <p className='mx-4 py-3'>{props.name}</p>
-        <p className='mx-7'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Blandit cursus risus at ultrices mi. Sed ullamcorper morbi tincidunt ornare massa. Cum sociis natoque penatibus et magnis dis. Blandit cursus risus at ultrices mi tempus imperdiet. Diam phasellus vestibulum lorem sed risus ultricies tristique. Tellus orci ac auctor augue mauris augue neque gravida. Imperdiet nulla malesuada pellentesque elit eget gravida. Venenatis tellus in metus vulputate. Magna etiam tempor orci eu lobortis elementum nibh tellus molestie. Sed euismod nisi porta lorem mollis aliquam.</p>
+        <p className='mx-7'>{props.description}</p>
         <div className="flex justify-between">
             <p className='m-3'>Date: {props.date}</p>
             <p className='m-3'>{props.location}</p>

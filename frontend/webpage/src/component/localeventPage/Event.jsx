c050821ce3065ea28c99db0942bb772e211b63cf
matthew0122 <mdb326@lehigh.edu>
2023-11-11 15:49:58 -0500

    Get comments from object

--- frontend/webpage/src/component/localeventPage/Event.jsx
+++ frontend/webpage/src/component/localeventPage/Event.jsx
@@ -1,5 +1,5 @@
 import styles from './styles.module.css';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import EventComment from './EventComment';
@@ -7,6 +7,18 @@ import Comments from './Comments.png';
 
 const Event = (props) => {
   const [showComments, setShowComments] = useState(false);
+
+  const [comments, setComments] = useState([]);
+
+  useEffect( () => {
+    setComments([...comments, {
+      "id": "1",
+      "user": "User 1",
+      "date": "11/09/2023", 
+      "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nunc consequat interdum varius sit amet mattis vulputate. Volutpat blandit aliquam etiam erat velit scelerisque in dictum non. Ut sem nulla pharetra diam sit amet nisl. Massa ultricies mi quis hendrerit dolor magna eget est. Integer eget aliquet nibh praesent tristique. Velit sed ullamcorper morbi tincidunt ornare massa eget egestas. Rhoncus est pellentesque elit ullamcorper dignissim cras tincidunt lobortis. Vel quam elementum pulvinar etiam. Convallis posuere morbi leo urna. Sed turpis tincidunt id aliquet risus feugiat in ante metus. Tortor vitae purus faucibus ornare. At augue eget arcu dictum varius duis."
+    }
+     ]);
+  }, []);
 
   const toggleComments = () => {
     setShowComments(!showComments);
@@ -24,7 +36,9 @@ const Event = (props) => {
         </div> 
     </div>
     { showComments &&
-        <EventComment user="User 1" date="11/09/2023" content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nunc consequat interdum varius sit amet mattis vulputate. Volutpat blandit aliquam etiam erat velit scelerisque in dictum non. Ut sem nulla pharetra diam sit amet nisl. Massa ultricies mi quis hendrerit dolor magna eget est. Integer eget aliquet nibh praesent tristique. Velit sed ullamcorper morbi tincidunt ornare massa eget egestas. Rhoncus est pellentesque elit ullamcorper dignissim cras tincidunt lobortis. Vel quam elementum pulvinar etiam. Convallis posuere morbi leo urna. Sed turpis tincidunt id aliquet risus feugiat in ante metus. Tortor vitae purus faucibus ornare. At augue eget arcu dictum varius duis."/>
+        comments.map(comment => (
+            <EventComment user={comment.user} date={comment.date} content={comment.content} key={comment.id} />
+        ))
     }
     </>
   )

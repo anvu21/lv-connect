44f084cb59a0fe15c9a9fcb66ce8e23b8f28dd29
anhvu21 <Vuphamducanh2000@gmail.com>
2023-11-12 10:27:20 -0500

    e

--- frontend/webpage/src/component/CreateAcct.jsx
+++ frontend/webpage/src/component/CreateAcct.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { motion } from 'framer-motion';
 import './AuthPage.css'; // Assuming you have basic CSS for layout
-import Navbar from "./Navbar.jsx";
 import { Link } from 'react-router-dom';
 import { useNavigate } from "react-router-dom";
 import axios from "axios";

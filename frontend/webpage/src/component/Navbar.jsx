7b60901d79243072f900742aa8a82217c12f90ac
matthew0122 <mdb326@lehigh.edu>
2023-11-11 22:37:46 -0500

    Clean up navbar

--- frontend/webpage/src/component/Navbar.jsx
+++ frontend/webpage/src/component/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
   return (
     <nav className="flex items-center justify-between p-4 bg-white shadow-md">
         
-        <div className="flex items-center"> {/* Flex container for logo and text */}
+      <div className="flex items-center"> {/* Flex container for logo and text */}
         <img src={logo} alt="logo" className="w-8 h-8 object-contain mr-2"/> {/* Logo with right margin */}
         <div className="text-xl font-bold">
           <Link to="/" style={{ color: '#00539b' }}>Lvconnect</Link>
@@ -34,10 +34,8 @@ const Navbar = () => {
     <Link to={link.path} className="hover:text-blue-600">{link.title}</Link>
         </li>
   ))}
-  <button className="flex items-center justify-center px-4 py-2 border rounded-full hover:bg-gray-100">
-          
-          
-          <span className="ml-2">Login</span>
+  <button className="flex items-center justify-center px-4 py-2 border rounded-full hover:bg-gray-100">  
+          <span className="ml-2"><Link to="/auth">Login</Link></span>
         </button>
 </ul>
 

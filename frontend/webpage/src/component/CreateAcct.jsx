c3706ddbeefbd1c091258edd4d25eacb3cfa23d1
matthew0122 <mdb326@lehigh.edu>
2023-11-12 00:19:34 -0500

    Add a separate create account page

--- frontend/webpage/src/component/CreateAcct.jsx
+++ frontend/webpage/src/component/CreateAcct.jsx
@@ -1,9 +1,8 @@
 import React, { useState } from 'react';
 import { motion } from 'framer-motion';
-import { Link } from 'react-router-dom';
 import './AuthPage.css'; // Assuming you have basic CSS for layout
 
-const AuthPage = () => {
+const CreateAcct = () => {
   const [isLogin, setIsLogin] = useState(true);
 
   const toggleForm = () => {
@@ -27,7 +26,7 @@ const AuthPage = () => {
     <div className="max-w-2xl mx-auto ">
     <div className="bg-white shadow-md border border-gray-200 rounded-lg max-w-sm p-4 sm:p-6 lg:p-8 ">
       <form className="space-y-6" action="#">
-        <h3 className="text-xl font-medium text-gray-900 ">Sign in to our platform</h3>
+        <h3 className="text-xl font-medium text-gray-900 ">Create an account!</h3>
         <div>
           <label htmlFor="email" className="text-sm font-medium text-gray-900 block mb-2 ">Your email</label>
           <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " placeholder="name@company.com" required />
@@ -47,7 +46,7 @@ const AuthPage = () => {
         </div>
         <button type="submit" className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center ">Login to your account</button>
         <div className="text-sm font-medium text-gray-500 ">
-          Not registered? <a href="#" className="text-blue-700 hover:underline "><Link to="/create-account">Create account</Link></a>
+          Not registered? <a href="#" className="text-blue-700 hover:underline ">Create account</a>
         </div>
       </form>
     </div>
@@ -57,4 +56,4 @@ const AuthPage = () => {
   );
 };
 
-export default AuthPage;
+export default CreateAcct;

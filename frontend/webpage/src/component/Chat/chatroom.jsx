c0fc724dd8f108d9a5d190fb03c119c8f23d9800
matthew0122 <mdb326@lehigh.edu>
2023-11-11 22:26:22 -0500

    Center volunteer content

--- frontend/webpage/src/component/Chat/chatroom.jsx
+++ frontend/webpage/src/component/Chat/chatroom.jsx
@@ -2,14 +2,18 @@ import styles from './styles.module.css';
 import React, { useState } from 'react';
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import NavBar from "../Navbar.jsx";
 
 const chatroom = () => {
   
  
   
   return (
-    <div>Local
+    <>
+    <NavBar />
+    <div>Chat Room
     </div>
+    </>
   )
 }
 

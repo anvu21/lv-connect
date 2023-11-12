0e593c7e62aab3f04d126576fedfadbd508549bc
anhvu21 <Vuphamducanh2000@gmail.com>
2023-11-12 11:21:28 -0500

    e

--- frontend/webpage/src/App.jsx
+++ frontend/webpage/src/App.jsx
@@ -3,7 +3,6 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 import Main from './component/mainpage/Main';
 import Local from './component/localeventPage/localEvent';
-import Business from './component/Business/business';
 
 import Chat from './component/Chat/chatroom';
 import Volunteer from './component/Volunteer/volunteer';
@@ -15,6 +14,7 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+//          <Route path="/volunteer-opportunities" element={<Volunteer />}/>
 
   return (
     <>
@@ -27,9 +27,7 @@ function App() {
           
           <Route path="/" element={<Navigate replace to="/main" />} />
           <Route path="/local" element={<Local />}/>
-          <Route path="/local-business-support" element={<Business />}/>
           <Route path="/chat-room" element={<Chat />}/>
-          <Route path="/volunteer-opportunities" element={<Volunteer />}/>
           <Route path="/*" element={<Navigate replace to="/main" />} />
           <Route path="/auth" element={<Auth />}/>
           <Route path="/create-account" element={<CreateAcct />}/>

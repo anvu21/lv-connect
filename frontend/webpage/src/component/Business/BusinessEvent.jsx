c9466f3377142471dc706c67fdae773aee57b17b
Anthony Gatti <anthony@dyn239021.cc.lehigh.edu>
2023-11-11 22:32:24 -0500

    Added business event page

---
+++ frontend/webpage/src/component/Business/BusinessEvent.jsx
@@ -0,0 +1,28 @@
+import styles from './styles.module.css';
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
+const BusinessEvent = (props) => {
+  
+ 
+  return (
+    <>
+    <div className='w-11/12 flex flex-col items-center' style={{marginLeft:'auto', marginRight:'auto'}}>
+        <div className="bg-gray-100 text-left rounded-lg mt-5 mb-3" style={{ color: '#00539b' }}>
+            <p className='mx-4 py-3'>{props.name}</p>
+            <p className='mx-7'>{props.description}</p>
+            <div className="flex justify-between">
+                <p className='m-3'>Date: {props.date}</p>
+                <p className='m-3'>{props.location}</p>
+                <p className='m-3'>Posted: {props.posted}</p>
+            </div> 
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default BusinessEvent;
+
+
